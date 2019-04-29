0bbe9424c005c16af596c66cbf7cb859b147bccc
hiaoxui <hiaoxui@gmail.com>
2019-04-30 01:33:39 +0800

    new codes

--- raw2all.py
+++ raw2all.py
@@ -20,6 +20,8 @@ def raw2all():
 
     pub_pattern = re.compile(r'((\n|^)@.*?)(?=(\n@|$))', re.S)
     entry_pattern = re.compile(r'(?=\n).*?\},(?=\n)', re.S)
+    book_pattern = re.compile(r'= \{(.*)\}')
+
     bib_items = pub_pattern.findall(text)
     print('{} bib items found.'.format(len(bib_items)))
     all_bib_items = list()
@@ -34,7 +36,6 @@ def raw2all():
         bib_type = first_line[1:bracket_idx]
         content = '\n' + content[first_n+1:-2] + ',\n'
 
-
         all_entries_raw = list(entry_pattern.findall(content))
         all_entries = list()
         for entry_raw in all_entries_raw:
@@ -46,11 +47,15 @@ def raw2all():
         this_item = list()
         for entry_name, entry in all_entries:
             if entry_name in accepted:
+                if entry_name in ['journal', 'booktitle']:
+                    book_name = book_pattern.findall(entry)[0]
+                    book_name = abbr2full(book_name)
+                    entry = r'{} = {{{}}}'.format(entry_name, book_name)
                 this_item.append(entry)
-        item_str = first_line + '\n' + ',\n'.join(this_item) + '}'
+        item_str = first_line + '\n' + ',\n'.join(this_item) + '\n}'
         all_bib_items.append(item_str)
     with open('all.bib', 'w') as fp:
-        fp.write('\n'.join(all_bib_items))
+        fp.write('\n\n'.join(all_bib_items))
 
 
 if __name__ == '__main__':
